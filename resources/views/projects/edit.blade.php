dd78d8e22a70e85d42ad2efb4f05dad49408357f
AIT <anas.tasadduq@gmail.com>
2019-06-20 09:07:20 +0300

    improve edit page style

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -1,40 +1,43 @@
-@extends('layouts.main')
+@extends('layouts.app')
 
-@section('title', 'Edit Project')
+{{-- @section('title', 'Edit Project') --}}
 
 @section('content')
 
-    <h1>Edit a Project</h1>
 
-    <hr />
 
     @include('partials.errors')
 
-    <form action="/projects/{{ $project->id }}" method="POST">
-        @csrf
+    <div class="card">
+        <div class="card-header"><h1>Edit a Project</h1></div>
+        <div class="card-body">
+            <form action="/projects/{{ $project->id }}" method="POST">
+                @csrf
 
-        @method('PATCH')
+                @method('PATCH')
 
-        <div>
-            <input type="text" name="title" value="{{ $project->title }}" placeholder="Project Title" />
+                <div>
+                    <input class="form-control" type="text" name="title" value="{{ $project->title }}" placeholder="Project Title" />
+                </div>
+                <br>
+                <div>
+                    <textarea class="textarea form-control" name="description" cols="30" rows="10" placeholder="Project Description">{{ $project->description }}</textarea>
+                </div>
+                <br>
+                <div>
+                    <button class="btn btn-primary" type="submit">Edit Project</button>
+                </div>
+            </form>
+            <br>
+            <form action="/projects/{{ $project->id }}" method="POST">
+                @csrf
+
+                @method('DELETE')
+
+                <div>
+                    <button class="btn btn-danger" type="submit">Delete Project</button>
+                </div>
+            </form>
         </div>
-
-        <div>
-            <textarea name="description" cols="30" rows="10" placeholder="Project Description">{{ $project->description }}</textarea>
-        </div>
-
-        <div>
-            <button class="edit" type="submit">Edit Project</button>
-        </div>
-    </form>
-
-    <form action="/projects/{{ $project->id }}" method="POST">
-        @csrf
-
-        @method('DELETE')
-
-        <div>
-            <button class="delete" type="submit">Delete Project</button>
-        </div>
-    </form>
+    </div>
 @endsection
